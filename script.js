dfdab91abacb95b36487c905c8b181ae42e834f3
Guilherme Bertuol <82103102+guilherme141288@users.noreply.github.com>
2023-04-08 15:41:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed (".multiple-text" , {
-    strings: ["FullStack Developer" , "English Coach"],
+    strings: ["Junior FullStack Developer" , "English Coach"],
     typeSpeed : 80,
     backSpeed : 30,
     backDelay : 1000,
